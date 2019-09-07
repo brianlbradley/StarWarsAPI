b4aa7ae73181db2001db2f22a487c9ec2cf8db6c
Brian Bradley <brianbradley@Brians-Air-2.attlocal.net>
2019-09-07 14:39:25 -0500

    Updates

---
+++ precache-manifest.ec465c5a448692dee431f7c8a2fcbd66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8434f8e5bb9b9ba7a23ce5e3c37d6016",
+    "url": "/StarWarsAPI/index.html"
+  },
+  {
+    "revision": "d0f6a0d8530c996034d8",
+    "url": "/StarWarsAPI/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3f5495ccc5e5f6d07048",
+    "url": "/StarWarsAPI/static/css/main.8384cb82.chunk.css"
+  },
+  {
+    "revision": "d0f6a0d8530c996034d8",
+    "url": "/StarWarsAPI/static/js/2.acc74ee0.chunk.js"
+  },
+  {
+    "revision": "3f5495ccc5e5f6d07048",
+    "url": "/StarWarsAPI/static/js/main.9d853238.chunk.js"
+  },
+  {
+    "revision": "d2082eaa04a0255e19eb",
+    "url": "/StarWarsAPI/static/js/runtime~main.efce55bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/StarWarsAPI/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8aa1767d11a854b6462c0b642c9aca8b",
+    "url": "/StarWarsAPI/static/media/starwars.8aa1767d.jpg"
+  }
+]);
